93759090e95360c53e9e3dcde9f4d2ee2d48a0c6
misha <twmisha093@mail.ru>
2014-12-07 19:37:38 +0200

    some sctipt

--- script.php
+++ script.php
@@ -1,4 +1,11 @@
 <?php
 
 
+header('Content-Type: application/json');
+ $db = mysql_connect ("localhost","root","123qweasdzxcv");
+ mysql_select_db("slava",$db);
+$query = 'SELECT * FROM users';
+$res  = mysql_query($query,$db);
+$result = mysql_fetch_array($res,MYSQL_ASSOC);
+echo "result";
 ?>
